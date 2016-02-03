20e8ca7fec262deb425b7ccd7f2b9bce99c59c4a
joeymilanes <jmilanes@wasabirabbit.com>
2016-02-02 22:30:18 -0500

    Removing Grunt

--- tasks/options/cssmin.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	target: {
-    files: {
-      'css/output.css': ['css/WRR_main.css', 'css/vertgrid.css']
-    }
-  }
-}
--- tasks/options/gh-pages.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-'gh-pages': {
-    options: {
-      branch: 'master',
-    },
-    src: ['**']
-  }
-}
